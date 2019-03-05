2def8fb65a03c971ec638eecdd9acbd1751fadc3
alex <alepabon@uat.edu>
2019-03-04 19:48:28 -0700

    Implemented Files
    added the SIP file into GitKraken and networking.

---
+++ SIP Voice Playback DemoV1/SIP Voice Playback Demo/Assets/Multiplayer/PlayerObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerObject : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+
+
+        Debug.Log("PlayerObject::Start -- Spawning my own personal unit.");
+        Instantiate(PlayerUnitPrefab);
+
+	}
+
+    public GameObject PlayerUnitPrefab;
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ SIP Voice Playback DemoV1/SIP Voice Playback Demo/Assets/Scripts/VoicePlaybackScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoicePlaybackScript : MonoBehaviour {
+
+   private AudioSource rAudio;
+
+	void Start ()
+    {
+           rAudio = gameObject.GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Debug.Log("Recording");
+            rAudio.clip = Microphone.Start("Blue Snowball", true, 10, 44100);
+        }
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            rAudio.Play();
+        }
+        foreach (string device in Microphone.devices)
+            Debug.Log(device);
+    }
+}
