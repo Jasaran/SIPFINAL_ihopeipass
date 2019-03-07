209f79975778d6e7b504ae8c748147f77c96ff60
Alex <alepabon@uat.edu>
2019-03-06 17:14:04 -0700

    Voice Affected Area WIP
    implemented a trigger box that will only say an object has entered or exited, and will work on converting that into a VAA.

---
+++ SIP Voice Playback DemoV1/SIP Voice Playback Demo/Assets/Scripts/VoiceAffectScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoiceAffectScript : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Object Entered the trigger");
+    }
+
+    void OntriggerStay(Collider other)
+    {
+        Debug.Log("object is within the trigger");
+    }
+
+    void OntriggerExit(Collider other)
+    {
+        Debug.Log("Object has exited the trigger");
+    }
+
+}
